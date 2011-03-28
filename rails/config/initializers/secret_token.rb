cd609ea104f84c34aa499614ee228f8bfea7d455
Aidan Feldman <aidan@jux.com>
2011-03-28 10:01:37 -0400

    added vanilla rails app

---
+++ rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OauthPluginTestApp::Application.config.secret_token = '84a8ec805710c6c7d3e4d44e864ae641dc3ec77aed89093c6046230f7e4fd6c32fbccfaea1166efe874cd65fe2adcd3367fa25549da308e07618b90a48a9b56c'
---
+++ rails/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+OauthPluginTestApp::Application.config.session_store :cookie_store, :key => '_OauthPluginTestApp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# OauthPluginTestApp::Application.config.session_store :active_record_store
